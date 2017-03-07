dc3d5d4ce68411f9b1a854045f5807d1b52c007e
hanjiayi <hanjiayi@yidian-inc.com>
2017-03-07 18:52:36 +0800

    judge img

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,7 @@
 /**
  * Created by humorHan on 2017/2/10.
  */
+var fs = require('fs');
 var path = require("path");
 var gulp = require('gulp');
 var tiny = require('gulp-tinypng');
@@ -13,11 +14,21 @@ var del = require('del');
  * 该插件会缓存上次压缩后的图片到old文件夹，然后会自动清空dist文件夹，然后把新压缩的文件放到dist文件夹
  * */
 
+//统计图片数量
+gulp.task('bundle', ['tinyPng'], function () {
+    fs.readdir(path.join(__dirname, 'src'), function (err, files) {
+        console.info('原图片：' + filterImg(files));
+    });
+    fs.readdir(path.join(__dirname, 'dist'), function (err, files) {
+        console.info('压缩成功：' + filterImg(files));
+    })
+});
+
 //压缩图片
-gulp.task('bundle', ['del'], function () {
+gulp.task('tinyPng', ['del'], function () {
     return gulp.src(path.join(__dirname, '/src/*.{png,jpg}'))
         .pipe(tiny('ESUKcbJt9lRkplqmVQLZ9Ot5uAeY2f95'))
-        .pipe(gulp.dest(path.join(__dirname,'/dist/')));
+        .pipe(gulp.dest(path.join(__dirname, '/dist/')));
 });
 
 //清理dist文件夹
@@ -29,7 +40,7 @@ gulp.task('del', ['old'], function () {
 //备份old
 gulp.task('old', ['del-old'], function () {
     return gulp.src(path.join(__dirname, '/dist/*.{png,jpg}'))
-        .pipe(gulp.dest(path.join(__dirname,'/old/')));
+        .pipe(gulp.dest(path.join(__dirname, '/old/')));
 });
 
 //清理old文件夹
@@ -37,3 +48,12 @@ gulp.task('del-old', function () {
     return gulp.src(path.join(__dirname, '/old'), {read: false})
         .pipe(vinylPaths(del));
 });
+
+function filterImg(files){
+    files.forEach(function (file, index) {
+        if (!/\.(png|jpeg|jpg|gif)$/.test(file)) {
+            files.splice(index, 1);
+        }
+    });
+    return files.length;
+}
